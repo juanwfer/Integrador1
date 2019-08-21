0bbb07fa5599f22ae9ce7cddbc74a241601e5dad
Juan Fernandez <juanwfer@gmail.com>
2019-08-20 23:21:15 -0300

    SmsReceiver.java + Nueva clase del tipo BroadcastReceiver
    MainActivity.java
    + Instanciar dinámicamente un SmsReceiver, y artificialmente ejecutar su método onReceive para probar

--- app/src/main/java/com/example/integrador1/MainActivity.java
+++ app/src/main/java/com/example/integrador1/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.integrador1;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SmsReceiver sr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        this.sr = new SmsReceiver();
+        registerReceiver(this.sr, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(this.sr);
+        this.sr.onReceive(this, new Intent("android.provider.Telephony.SMS_RECEIVED"));
+        super.onPause();
     }
 }
